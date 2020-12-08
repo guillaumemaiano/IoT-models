f47a7d8e786fe32d74b8dbb21dc8b0647ca9ab7e
Guillaume Maiano <guillaume@maiano.fr>
2020-12-08 23:09:31 +0100

    A basic, blocking version of the R2 droid light

--- xwingbase/Hardware/Sketches/Base.c
+++ xwingbase/Hardware/Sketches/Base.c
@@ -43,9 +43,46 @@ void bunkerLights() {
 
 }
 
-void droidLight() {
-  int maxDuration = 5000; // max duration of a cycle in milliseconds
-  int maxPoints = 15; //
-  randomSeed();
-  //beeps = random(0,15);
+void droidLights() {
+  // Init consts
+  const int defaultDuration = 270; // ceil(4 seconds /15 points)
+  const int maxDuration = 4000; // max duration of a cycle in milliseconds
+  const int maxPoints = 15; // max occurrences of a "point" (up signal of varying length)
+  const int durations[3] = {100, 300, 500}; // possible durations of a point (short, medium, long)
+  const int separationInterval = 100; // a millisecond duration to separate "points" visually
+  // prep randoms
+  randomSeed(analogRead(0));
+  int beeps = random(1, maxPoints);
+  int points[beeps];
+  for (int point = 0; point < beeps; point++) {
+        int pointLength = random(0,4); // if 3, then point is a blank
+        if (pointLength == 3) {
+                points[point] = 0; // standard empty point is 270ms long
+        } else {
+          points[point] = durations[pointLength];
+        }
+  }
+  // execute blocking basic loop
+  // Turn off Droid LED
+  digitalWrite(blueLED_Xwing,
+        LOW);
+  int currentDuration = 0;
+  for (int currentPoint = 0; currentPoint < beeps && currentDuration < maxDuration; currentPoint++) {
+        if (points[currentPoint] == 0){
+                // make sure that led *is* off, as it should be
+                digitalWrite(blueLED_Xwing,
+                LOW);
+                delay(defaultDuration);
+                currentDuration += defaultDuration;
+        } else {
+                int value = points[currentPoint];
+                digitalWrite(blueLED_Xwing,
+                HIGH);
+                delay(value);
+                currentDuration += value;
+                digitalWrite(blueLED_Xwing,
+                LOW);
+                delay(separationInterval);
+        }
+  }
 }
